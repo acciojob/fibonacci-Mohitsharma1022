54f44a173d8f65e4555e2baeaeeb24a77a5b11db
Mohit Sharma <167015033+Mohitsharma1022@users.noreply.github.com>
2025-01-17 00:57:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
 // your code here
+	if(num<=2){
+		return n-1;
+	}
+	let a=0, b=1;
+	while(num > 2){
+		let curr = a+b;
+		    a = b;
+		    b = curr;
+		    n--;
+	}
+	return b;
 }
 
 module.exports = fibonacci;
